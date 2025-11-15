2fbb0d443c5bdb94392a3ee97ce0797e92fb51e9
Apurv Jyotirmay <mail@jyotirmay.dev>
2025-11-15 17:20:36 +0530

    updated namespace

--- backup.php
+++ backup.php
@@ -1,6 +1,7 @@
 <?php
 require __DIR__ . '/_init.php';
-$backup = new \Tribe\Backup;
+$backup = new \App\Backup;
+
 $backup->mysqlDatabase();
 $backup->uploadsFolder();
 $backup->deleteOldMySQLBackups();
